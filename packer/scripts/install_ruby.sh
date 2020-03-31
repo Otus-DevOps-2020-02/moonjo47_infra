6cd15a363349aad1104ecbf4c80533f12cd0d93e
moonjo <moonjo@yandex.ru>
2020-03-31 21:20:22 +0300

    merge

---
+++ packer/scripts/install_ruby.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Install ruby
+apt update
+apt install -y ruby-full ruby-bundler build-essential
+
+
+#!/bin/sh
+#sudo apt update
+#sudo apt install -y ruby-full ruby-bundler build-essential
+#ruby -v
+#bundler -v
