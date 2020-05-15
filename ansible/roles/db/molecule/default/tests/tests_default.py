8f9e2d88ede803c6f44d41d764acd7bfee20de9a
moonjo <moonjo@yandex.ru>
2020-05-16 02:14:11 +0300

    added test mongo

--- ansible/roles/db/molecule/default/tests/tests_default.py
+++ ansible/roles/db/molecule/default/tests/tests_default.py
@@ -16,3 +16,6 @@ def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+def test_mongo_is_listening_on_port(host):
+    assert host.socket("tcp://27017")
